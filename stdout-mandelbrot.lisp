5dfba977989bc9a3ca85187100adcf800c083b3e
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2023-11-12 12:34:04 -0700

    Remove type declarations because they didn't help (?!?)
    Quite surprised to get faster results without the type declarations.
    Maybe I made them too broad compared to what the compiler decides?
    Maye there's a cached optimization setting or something?

--- stdout-mandelbrot.lisp
+++ stdout-mandelbrot.lisp
@@ -15,12 +15,10 @@
 
 (in-package :stdout-mandelbrot)
 
-(declaim (optimize (speed 3) (space 0) (safety 0) (debug 0)))
-
 (defun mandelbrot ()
   (let* ((stream t)
-         (width 1600)
-         (height 600)
+         (width 160)
+         (height 60)
          (min #C(-2.1d0 -1.2d0))
          (max #C(0.6d0 1.2d0))
          (iterations 32)
@@ -35,39 +33,26 @@
                             (/ (imagpart (- max min))
                                height))))
 
-    ;; Type declarations to help find errors and improve performance
-    (declare (type fixnum width height iterations)
-             (type string colors)
-             (type (complex (double-float)) min max inc-real inc-imag))
-
     ;; imaginary part on the y axis
     (loop
-      :for row fixnum :below height
-      :for row-val of-type (complex (double-float)) = min :then (+ row-val inc-imag)
+      :for row :below height
+      :for row-val = min :then (+ row-val inc-imag)
       :do
          ;; Real part on the x axis
          (loop
-           :for col fixnum :below width
-           ;; These are a little messy with the type declarations
-           :for c of-type (complex (double-float))
-             = row-val :then (+ c inc-real)
-
-           :for iter-count fixnum
-             = (loop
-                 :for current-iteration fixnum :below iterations
-                 :for z of-type (complex (double-float))
-                   = (complex 0.0d0 0.0d0)
-                     :then (+ (* z z) c)
-                 :until (> (abs z) 2.0d0)
-                 :finally (return current-iteration))
-
-           :for color-idx fixnum
-             = (floor
-                (* (1- (length colors))
-                   (/ iter-count
-                      iterations)))
-           :do
-              (format stream "~a" (aref colors color-idx)))
+           :for col :below width
+           :for c = row-val :then (+ c inc-real)
+           :for iter-count = (loop
+                               :for current-iteration :below iterations
+                               :for z = (complex 0.0d0 0.0d0)
+                                 :then (+ (* z z) c)
+                               :until (> (abs z) 2.0d0)
+                               :finally (return current-iteration))
+           :for color-idx = (truncate
+                             (* (1- (length colors))
+                                (/ iter-count
+                                   iterations)))
+           :do (format stream "~a" (aref colors color-idx)))
          (format stream "~%"))))
 
 (defun main (args)
